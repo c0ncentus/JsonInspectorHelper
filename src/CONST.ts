c1cc6a5019bc717d9f95097441a1932c55759f72
c0ncentus <jeremymartin33610@gmail.com>
2021-07-18 14:48:16 +0200

    up

---
+++ src/CONST.ts
@@ -0,0 +1,36 @@
+import { GlobalJIPSetting } from "./Model"
+
+export const JsonInspectSettings: GlobalJIPSetting = {
+    start: {
+        css: {
+            autoFillDangerous: false,
+            NEW: true,
+            ONLY_READ: false,
+            UPDATE_EXIST: { keys: true, value: true },
+            UPDATE_NEW: { keys: true, value: true }
+        }
+    },
+    training: {
+        autoFillDangerous: false,
+        NEW: true,
+        ONLY_READ: false,
+        UPDATE_EXIST: { keys: true, value: true },
+        UPDATE_NEW: { keys: true, value: true }
+    }
+}
+
+export const INIT_VALUES_BY_TYPE = {
+    word: "",
+    color: "rgb(255,255,255)",
+    img: "https://i.redd.it/q7jrzsv9ri521.png",
+    https: "https://i.redd.it/q7jrzsv9ri521.png",
+    http: "http://jyamg.chezsuz.com/wp-content/uploads/2013/11/JYAMG-brentrup-mandoline-011.jpg",
+    number: 0,
+    array: [],
+    object: {},
+    boolean: false,
+    undefined: undefined,
+    null: null,
+}
+
+export const colorDeep = ["#32be32", "#3281be", "#8b32be", "#be3232", "#e6e600", "#e69600"]
